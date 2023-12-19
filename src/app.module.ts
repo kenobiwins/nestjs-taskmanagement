77d0f67acae46efa0b771a3f57a0240381fdec21
Maksym Sydorenko <maksym.sy96@gmail.com>
2023-12-19 21:03:32 +0200

    feat: implement auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TasksModule,
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true,
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: process.env.DB_AUTOLOAD_ENTITIES === 'true',
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
     }),
+    AuthModule,
   ],
 })
 export class AppModule {}
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersRepository } from './users.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { JwtPayload } from './jwt-payload.interface';
+import { User } from './user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+  ) {
+    super({
+      secretOrKey: process.env.SECRET_KEY,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(paayload: JwtPayload): Promise<User> {
+    const { username } = paayload;
+    const user: User = await this.usersRepository.findOne({ username });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
